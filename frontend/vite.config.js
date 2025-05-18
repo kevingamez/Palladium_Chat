4ddfc164865ba3960f87baae6f983054f0b90afd
kevingamez <ks.gamez@uniandes.edu.co>
2025-05-17 19:06:52 -0500

    fix vite

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,25 +1,29 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
-import { tamaguiPlugin } from '@tamagui/vite-plugin'
-import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
-import { resolve } from "node:path";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
+import { tamaguiPlugin } from '@tamagui/vite-plugin';
+import { TanStackRouterVite } from '@tanstack/router-plugin/vite';
+import { resolve } from 'node:path';
 
 export default defineConfig({
   plugins: [
     TanStackRouterVite({ autoCodeSplitting: true }),
     react(),
     tailwindcss(),
-    tamaguiPlugin({ config: 'tamagui.config.ts', components: ['tamagui'], optimize: true,})
+    tamaguiPlugin({
+      config: 'tamagui.config.ts',
+      components: ['tamagui'],
+      optimize: false,
+    }),
   ].filter(Boolean),
   test: {
     globals: true,
-    environment: "jsdom",
+    environment: 'jsdom',
   },
   resolve: {
     alias: {
       '@': resolve(__dirname, './src'),
       'react-native': 'react-native-web',
     },
-  }
+  },
 });
